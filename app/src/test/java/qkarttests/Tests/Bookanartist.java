38f27a51db12e3ddc3f00710dfda87ca3e9a5a7c
Pratik Bhowmik <pratikbhowmik@Pratiks-MacBook-Air.local>
2024-05-27 15:17:54 +0600

    new code

--- app/src/test/java/qkarttests/Tests/Bookanartist.java
+++ app/src/test/java/qkarttests/Tests/Bookanartist.java
@@ -20,20 +20,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Bookanartist {
     public static void main(String[] args) throws InterruptedException {
 
-        //This is the script for posting a job with user logged in
-        //Please create a dummy account with dummy credentials to see the script running also email should be verified
-        //Also the captcha automation is not recommended so please solve the captcha manually to see the script running
-        //I have also included validations
-        //Script will stop in b/w you have to enter solved in the console to continue the script
-        //All the steps are written below
-
-        
-
+        // This is the script for posting a job with user logged in
+        // Please create a dummy account with dummy credentials to see the script
+        // running also email should be verified
+        // Also the captcha automation is not recommended so please solve the captcha
+        // manually to see the script running
+        // I have also included validations
+        // Script will stop in b/w you have to enter solved in the console to continue
+        // the script
+        // All the steps are written below
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
 
         driver.manage().window().maximize();
         driver.get("https://hireanartist.com.au/post-a-job");
@@ -42,20 +41,19 @@ public class Bookanartist {
         // driver.findElement(By.cssSelector(".ant-btn.css-6rzz9k.ant-btn-default.custom-btn.custom-btn__regular.btn__outline-secondary")).click();
         // driver.findElement(By.id("email")).sendKeys("impratikbhowmik@gmail.com");
         // driver.findElement(By.id("password")).sendKeys("abc@97");
-        
 
-        // This code is for captcha but 
-        // it's not recommended to do automation for captcha, 
-        // captcha is designed to prevent automation 
-        // please solve the captcha manually 
-        // to see the rest of the script running in action please input SOLVED on your terminal to see the script running
+        // This code is for captcha but
+        // it's not recommended to do automation for captcha,
+        // captcha is designed to prevent automation
+        // please solve the captcha manually
+        // to see the rest of the script running in action please input SOLVED on your
+        // terminal to see the script running
         // Thread.sleep(6000);
         // driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@title='reCAPTCHA']")));
         // Thread.sleep(6000);
         // driver.findElement(By.cssSelector(".recaptcha-checkbox-border")).click();
         // driver.switchTo().defaultContent();
         // Thread.sleep(6000);
-
 
         // System.out.println("Please enter SOLVED once captcha is solved manually");
         // Scanner scan = new Scanner(System.in);
@@ -65,11 +63,12 @@ public class Bookanartist {
 
         // Thread.sleep(6000);
 
-        // List<WebElement> similiarele = driver.findElements(By.cssSelector(".ant-btn.css-6rzz9k.ant-btn-default.custom-btn.custom-btn__regular.btn__solid-tertiary"));
+        // List<WebElement> similiarele =
+        // driver.findElements(By.cssSelector(".ant-btn.css-6rzz9k.ant-btn-default.custom-btn.custom-btn__regular.btn__solid-tertiary"));
 
         // similiarele.get(0).click();
 
-        //Screen 1
+        // Screen 1
         driver.findElement(By.id("project_name")).sendKeys("Public Window Mural - Canada");
         driver.findElement(By.xpath("//span[text()='Mural/Graffiti']")).click();
 
@@ -94,23 +93,21 @@ public class Bookanartist {
         driver.findElement(By.xpath("//span[text()='Residential']")).click();
         driver.findElement(By.xpath("//span[text() = 'Continue']")).click();
 
-
-        //Screen 2
+        // Screen 2
         driver.findElement(By.xpath("//input[@value='No idea']")).click();
         driver.findElement(By.id("job_description")).sendKeys("We are seeking an experienced mural artist to create\n" + //
-                        "a captivating mural for a public window space measuring 5ft x 5ft in Canada.");
+                "a captivating mural for a public window space measuring 5ft x 5ft in Canada.");
         driver.findElement(By.xpath("//span[text()='Portrait']")).click();
 
-        Thread.sleep(3000);
+        Thread.sleep(6000);
 
         String filepath = "/Users/pratikbhowmik/Downloads/dreams.png";
-        driver.findElement(By.xpath("//button[@title = 'Attach file']")).sendKeys(filepath);
+        driver.findElement(By.xpath("//button[@title ='Attach file']")).sendKeys(filepath);
+        Thread.sleep(8000);
+        
+        driver.findElement(By.xpath("//span[text()='Continue']")).click();
 
-
-        driver.findElement(By.cssSelector(".ant-btn.css-6rzz9k.ant-btn-primary.custom-btn.custom-btn__long.btn__solid-primary")).click();
-
-
-        //Screen 3
+        // Screen 3
         driver.findElement(By.id("budget")).sendKeys("100.50");
         driver.findElement(By.id("currency")).sendKeys("CAD");
         Thread.sleep(6000);
@@ -122,12 +119,16 @@ public class Bookanartist {
             }
         }
 
-        driver.findElement(By.cssSelector(".ant-btn.css-6rzz9k.ant-btn-default.custom-btn.project-date-custom-btn")).click();
-        driver.findElement(By.cssSelector(".ant-btn.css-6rzz9k.ant-btn-primary.custom-btn.custom-btn__long.btn__solid-primary")).click();
+        driver.findElement(By.cssSelector(".ant-btn.css-6rzz9k.ant-btn-default.custom-btn.project-date-custom-btn"))
+                .click();
+        driver.findElement(
+                By.cssSelector(".ant-btn.css-6rzz9k.ant-btn-primary.custom-btn.custom-btn__long.btn__solid-primary"))
+                .click();
 
-        //Budget details
+        // Budget details
         Thread.sleep(6000);
-        List<WebElement> elements = driver.findElements(By.cssSelector(".ant-radio-button-wrapper.ant-radio-button-wrapper-in-form-item.css-6rzz9k"));
+        List<WebElement> elements = driver.findElements(
+                By.cssSelector(".ant-radio-button-wrapper.ant-radio-button-wrapper-in-form-item.css-6rzz9k"));
         elements.get(1).click();
 
         driver.findElement(By.xpath("//select//option[@value = 'IN']")).click();
@@ -150,7 +151,7 @@ public class Bookanartist {
 
         Thread.sleep(6000);
 
-        //Validation of thank you message after job is created  
+        // Validation of thank you message after job is created
         Assert.assertTrue(driver.findElement(By.id("poj-success-popup-gtm")).isDisplayed());
         driver.quit();
     }
